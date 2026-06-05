8d8e297acb88803b547908f542257b1149ba0abb
Nick Guarino <nguarino528@gmail.com>
2026-06-04 21:04:45 -0400

    Update cors policy

--- rest/src/main/java/payroll/config/CorsConfig.java
+++ rest/src/main/java/payroll/config/CorsConfig.java
@@ -13,7 +13,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://imageconvertapp.com"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(false);
